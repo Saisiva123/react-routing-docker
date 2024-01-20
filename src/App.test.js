b59b396757d19047e53679681f6aa6ea0e03d4e5
Saisiva123 <mannem.sai99@gmail.com>
2024-01-19 22:03:15 -0600

    updated the test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import ReactDOM from 'react-dom';
+import Home from './Pages/Home'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Home />, div);
 });
