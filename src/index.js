66aaadb85ab8aa28ac511649a68e2267be683c6b
Saisiva123 <mannem.sai99@gmail.com>
2024-01-19 18:32:49 -0600

    initial commit for docker

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Link to = "/">Home</Link>
+    <Link to = "/">Home ok</Link>
     <Link to = "/about/1">About</Link>
       <Routes>
         <Route path="/" element={<Home />} />
